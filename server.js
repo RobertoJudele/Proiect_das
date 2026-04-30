ee4811665b4cee8dacaed606eac2e7497574db7d
Robi <judeleroberto@gmail.com>
2026-04-30 15:50:39 -0400

    swithcing to v1

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
-// server.js - Pornire server
 const app = require('./src/app');
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`[v1-vulnerable] Server pornit pe http://localhost:${PORT}`);
